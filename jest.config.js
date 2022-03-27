045e14bed984283bf8cb9447be48891579343781
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-27 07:02:19 -0300

    ci: add setup ci

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
-export const coverageDirectory = 'coverage'
-export const collectCoverage = true
-export const collectCoverageFrom = [
-  'src/**/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
-  '!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}',
-  '!<rootDir>/node_modules/',
-]
-export const testEnvironment = 'jsdom'
-export const testMatch = ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
-export const testPathIgnorePatterns = ['/node_modules/', '/.next/', '/.out/', '/public/']
-export const setupFilesAfterEnv = ['<rootDir>/.jest/setup-tests.js']
+// eslint-disable-next-line no-undef
+module.exports = {
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+  ],
+
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
+
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+}
