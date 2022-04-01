d99fb0ea10d7c1f90a8db1269878830050de8ff1
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-04-01 18:03:40 -0300

    feat: add component MenuLink

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,64 @@
+import { screen } from '@testing-library/react'
+import { MenuLink } from '.'
+import { renderTheme } from '../../styles/render-theme'
+
+describe('<MenuLink />', () => {
+  it('Should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">Children</MenuLink>)
+    expect(screen.getByRole('link', { name: 'Children' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute('target', '_self')
+  })
+
+  it('Should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink newTab={true} link="http://localhost">
+        Children
+      </MenuLink>,
+    )
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute('target', '_blank')
+  })
+
+  it('Should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink newTab={false} link="http://localhost">
+        Children
+      </MenuLink>,
+    )
+    expect(screen.getByRole('link', { name: 'Children' })).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        color: #0A1128;
+        position: relative;
+      }
+
+      .c0::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        width: 0;
+        height: 0.2rem;
+        background: #2F5233;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      <a
+        class="c0"
+        href="http://localhost"
+        target="_self"
+      >
+        Children
+      </a>
+    `)
+  })
+})
--- src/components/TextComponent/stories.jsx
+++ src/components/TextComponent/stories.jsx
@@ -6,12 +6,7 @@ export default {
   args: {
     children: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
       Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur 
-      voluptatum laboru numquam blanditiis harum quisquam eius sed odit fugiat iusto 
-      fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. 
-      Veritatis Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, 
-      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laboru 
-      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, 
-      eaque rerum! Provident similique accusantium nemo autem. Veritatis`,
+      voluptatum laboru numquam blanditiis harum quisquam eius seds`,
   },
   argsTypes: {
     children: { type: 'string' },
